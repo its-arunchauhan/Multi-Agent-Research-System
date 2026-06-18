54aa08cdb996d78c01def2739de7dff38af526d9
Arun Chauhan <arun.chauhan.dev@gmail.com>
2026-06-18 12:53:43 +0000

    Tools created

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from src.pipelines.pipeline import run_research_pipeline
+# from src.pipelines.pipeline import run_research_pipeline
+from src.tools.tools import web_search, scrape_url
 
-
-topic = "The impact of AI on the job market in 2026"
-run_research_pipeline(topic)
+result = scrape_url(" https://www.nytimes.com/spotlight/artificial-intelligence")
+print(result)
+# topic = "The impact of AI on the job market in 2026"
+# run_research_pipeline(topic)
